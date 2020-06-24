39f89b517e006994e4a958653a466d153eebb205
Sven <sven@d-nite.de>
2020-06-24 22:27:02 +0200

    initial production build version

---
+++ precache-manifest.9d235e0603960ef3c6285c3751a404aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e89b6afbfcad8709ebca7a015f6a73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db2b3933709b1e52534c",
+    "url": "/static/css/2.389269c8.chunk.css"
+  },
+  {
+    "revision": "839447ba3b550b61867f",
+    "url": "/static/css/main.27533e26.chunk.css"
+  },
+  {
+    "revision": "db2b3933709b1e52534c",
+    "url": "/static/js/2.ed1388a7.chunk.js"
+  },
+  {
+    "revision": "2ff0ac40717a6a80714357135d1e1084",
+    "url": "/static/js/2.ed1388a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "839447ba3b550b61867f",
+    "url": "/static/js/main.9edf5098.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
